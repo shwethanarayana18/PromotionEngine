345a96a8f387252e272e34541783b370038a16f3
shwethanarayana18 <85767820+shwethanarayana18@users.noreply.github.com>
2021-06-12 11:28:52 +0200

    PromotionConst.java
    PromotionConst has all the constant values

---
+++ src/com/main/PromotionConsts.java
@@ -0,0 +1,20 @@
+package com.main;
+
+public interface PromotionConsts {
+	int priceOfA = 50;
+	int priceOfB = 30;
+	int priceOfC = 20;
+	int priceOfD = 15;
+	int discountRateOnAs=130;
+	int discountRateOnBs=45;
+	int discountOnComboCandD=30;
+	int numberOfAsdiscountAplicableOn=3;
+	int numberOfBsdiscountAplicableOn=2;
+	int numberOfCandDdiscountAplicableOn=2;
+	String AId="A";
+	String BId="B";
+	String CId="C";
+	String DId="D";
+	
+	
+}
